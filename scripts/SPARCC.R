1f8480cab45ecd338b36f746ef81c67a7155abce
rmondav <12041885+rmondav@users.noreply.github.com>
2021-05-25 13:16:32 +0200

    add scripts for sparcc analyses

---
+++ scripts/SPARCC.R
@@ -0,0 +1,54 @@
+library(Matrix); library(igraph)
+#library(SpiecEasi) #for local analyses
+library(SpiecEasi, lib.loc='/proj/snic2020-16-196/private/') #HPC analyses
+
+## make list for treatment names
+tn = list("MC_anO2_lake","MC_anO2_pond","MC_O2_lake","TS_lake_2000")
+## set up for loop to iterate over list "tn"
+for (tmnt in tn) { 
+  #read in files
+  infile=paste0("processed_tables/",tmnt,"_clean_otus.csv")
+  clean_otus<-read.csv(file =infile, header=T, row.names=1); rm(infile)
+  ## transpose
+  clean_otust<-t(clean_otus); #rm(clean_otus)
+  ## run sparcc
+  sparcc.obj <- sparcc(clean_otust); #rm(clean_otust)
+  ## replace names of OTUs as removed during processing
+  rownames(sparcc.obj$Cor) <- colnames(clean_otust)
+  colnames(sparcc.obj$Cor) <- colnames(clean_otust)
+  rownames(sparcc.obj$Cov) <- colnames(clean_otust)
+  colnames(sparcc.obj$Cov) <- colnames(clean_otust)
+  ## make correlation results accessible, convert to matrix
+  sparcc.corr.m<-as.matrix(sparcc.obj$Cor,dimnames(
+    rownames(sparcc.obj$Cor),colnames(sparcc.obj$Cor)))
+  ## convert to igraph object with named columns
+  sparcc_corr.adj<-graph.adjacency(sparcc.corr.m,mode="undirected",
+                  weighted=TRUE,diag=FALSE,add.colnames=NULL)
+  ## convert to dataframe
+  sparcc_corr.df<-get.data.frame(sparcc_corr.adj)
+  ## change correlation column name
+  colnames(sparcc_corr.df)[colnames(sparcc_corr.df)=="weight"]<-"sparcc.corr"
+  ## calculate pseudo pvals using reboot method
+  sparcc.p.out <- sparccboot(clean_otust, R = 1000, ncpus = 4)
+  ## convert results to list
+  sparcc.P <- pval.sparccboot(sparcc.p.out, sided = "both")
+  ## convert list to dataframe
+  sparcc.P_df<-as.data.frame(sparcc.P)
+  ## add pseudo pvals to corr matrix and rename
+  sparcc_corr.df$sparcc.ppvals<-sparcc.P_df$pvals
+  ## remove edges with (arbitrary) correlation weaker than |0.3|
+  sparcc_corr.rdcd<-
+    subset(sparcc_corr.df, sparcc.corr>0.3 | sparcc.corr<(-0.3))
+  ## remove those with high-ish probability of false positive
+  sparcc_corr.clean<-subset(sparcc_corr.rdcd, sparcc.ppvals<0.05)
+  ## make column with named edges
+  sparcc_corr.clean$edge<-
+    paste(sparcc_corr.clean$from,"to",sparcc_corr.clean$to,sep="_")
+  ## reorder columns so edge is first
+  sparcc_edges <- sparcc_corr.clean[,c(5,3,4,1,2)] 
+  ## save file as csv
+  outfile=paste0("results/",tmnt,"_sparcc_edges.csv")
+  ## save file as csv
+  write.csv(sparcc_edges, file=outfile, quote=F)
+}
+
---
+++ scripts/sbatch_SPARCC.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+#SBATCH -A snic2020-5-529
+#SBATCH -J sparcc
+#SBATCH -t 6:30:00
+#SBATCH -p core -n 1
+
+module load bioinfo-tools R/4.0.4 R_packages/4.0.4
+maindir="/proj/snic2020-16-196/nobackup/CHtrophy_network_nobackup/methanotrophy_networks"
+cd $maindir/
+Rscript --no-restore --no-save "$maindir/scripts/SPARCC.R"
+
