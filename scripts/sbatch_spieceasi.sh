7ff73a5f9800f1e94234144948580fa9847086bc
rmondav <12041885+rmondav@users.noreply.github.com>
2021-05-25 12:50:34 +0200

    update spieceasi slurm script

--- scripts/sbatch_spieceasi.sh
+++ scripts/sbatch_spieceasi.sh
@@ -1,12 +1,14 @@
 #! /bin/bash
 
-#SBATCH -A snic2020-15-261
+#SBATCH -A snic2020-5-529
 #SBATCH -J spiec.easi
-#SBATCH -t 4:00:00
+#SBATCH -t 4:30:00
 #SBATCH -p core -n 4
 
 module load bioinfo-tools R/4.0.4 R_packages/4.0.4
-datadir="/proj/snic2020-16-196/nobackup/CHtrophy_network_nobackup/methanotrophy_networks/processed_tables/"
-cd $datadir
-Rscript --no-restore --no-save SPIEC-EASI.R
+maindir="/proj/snic2020-16-196/nobackup/CHtrophy_network_nobackup/methanotrophy_networks"
+cd $maindir/processed_tables/
+Rscript --no-restore --no-save "$maindir/scripts/SPIEC-EASI.R"
+cd ../
+Rscript --no-restore --no-save convert_SE_results2df.R
 
