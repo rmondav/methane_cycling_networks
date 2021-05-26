eb0e5e11cedcca3fd24252b80f2d26007b520b6d
rmondav <12041885+rmondav@users.noreply.github.com>
2021-05-26 13:12:11 +0200

    add scripts for cleaning up tax tables

---
+++ scripts/process_OTU_tax_tables_for_plotting.R
@@ -0,0 +1,48 @@
+## separate OTUtax tables and add in abundance info for network plotting
+## make sample list
+tn <-c("MC_anO2_lake","MC_O2_lake","TS_lake_2000")
+## set up for loop to iterate over list "tn"
+for (tmnt in tn) {
+  #read in files
+  infile=paste0("processed_tables/",tmnt,"_clean_otus.csv")
+  clean_otus<-read.csv(file =infile, header=T, row.names=1)
+  ## make new df
+  names<-rownames(clean_otus)
+  ## make new df
+  otu_df<-as.data.frame(names)
+  ## get sum of r.a. abundances for each
+  otu_df$sum.r.a<-rowSums(clean_otus)
+  ## get prevalence for each
+  otu_df$no.samples<-rowSums(clean_otus!=0)
+  ## calculate mean r.a. abundance for each
+  otu_df$av.r.a.abund<-(otu_df$sum.r.a*100/2000)/(otu_df$no.samples)
+  ## calculate percentage of samples for each
+  otu_df$pcnt.samples<-otu_df$no.samples*100/(ncol(clean_otus))
+  ## save tables 
+  out_OTU=paste0("results/",tmnt,"_OTU_table_ntwk.csv")
+  write.csv(otu_df, file=out_OTU, quote=F, row.names=F)
+}  
+
+
+## reduce OTU table list that holds taxonomy and metabolic information
+## to make it easier to work with in making figures etc
+## read in full OTU table
+full_OTU<-read.csv("results/full_taxonomy_table.csv", header=T, row.names = 1)
+## get list of OTUs in the clean OTU tables
+infile="processed_tables/MC_anO2_lake_clean_otus.csv"
+clean_otus<-read.csv(file =infile, header=T, row.names=1) 
+names1<-rownames(clean_otus)
+infile="processed_tables/MC_O2_lake_clean_otus.csv"
+clean_otus<-read.csv(file =infile, header=T, row.names=1) 
+names2<-rownames(clean_otus)
+infile="processed_tables/TS_lake_2000_clean_otus.csv"
+clean_otus<-read.csv(file =infile, header=T, row.names=1) 
+names3<-rownames(clean_otus)
+## make list
+names123<-c(names1,names2,names3)
+names123U<-unique(names123)
+## subset full OTU table using list
+ntwk_OTU_df<-full_OTU[names123U,]
+## save table
+write.csv(ntwk_OTU_df, file="results/ntwk_OTU_tax_metab.csv")
+
--- scripts/sbatch_pearson_N_makeUnions.sh
+++ scripts/sbatch_pearson_N_makeUnions.sh
@@ -2,13 +2,14 @@
 
 #SBATCH -A snic2020-5-529
 #SBATCH -J pearsNunion
-#SBATCH -t 0:30:00
+#SBATCH -t 00:30:00
 #SBATCH -p core -n 1
 
 module load bioinfo-tools R/4.0.4 R_packages/4.0.4
 maindir="/proj/snic2020-16-196/nobackup/CHtrophy_network_nobackup/methanotrophy_networks"
 cd $maindir/
-Rscript --no-restore --no-save "$maindir/scripts/Pearson_correlation.R" &&
-Rscript --no-restore --no-save "$maindir/scripts/make_unioned_tables.R"
+Rscript --no-restore --no-save "scripts/Pearson_correlation.R" &&
+Rscript --no-restore --no-save "scripts/make_unioned_tables.R"
+Rscript --no-restore --no-save "scripts/process_OTU_tax_tables_for_plotting.R"
 
 
