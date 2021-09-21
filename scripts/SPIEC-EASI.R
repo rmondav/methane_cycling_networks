3b3d3e0a08a11a1da02cb2c76c74ff82429985c1
rmondav <12041885+rmondav@users.noreply.github.com>
2021-09-21 12:37:24 +0200

    add rds conversion to end of spieceasi script

--- scripts/SPIEC-EASI.R
+++ scripts/SPIEC-EASI.R
@@ -3,9 +3,7 @@
 #install_github("zdk123/SpiecEasi", lib="/proj/snic2020-16-196/private/")
 library(SpiecEasi, lib.loc='/proj/snic2020-16-196/private/')
 library(phyloseq)
-
-## set processing parameters and add seed for reproducibility
-paramargs <- list(rep.num=1000, seed=261, ncores=4)
+library(igraph)
 
 ## read in biom files and parse taxonomy information
 MC_anO2_lake1_clean_psob<-import_biom(
@@ -18,7 +16,11 @@ TS_lake_2000_clean_psob<-import_biom(
   "OTU_table_TS_lake_2000_min18S.ovr38count.biom",
   parseFunction=parse_taxonomy_default)
 
+## set spieceasi processing parameters and add seed for reproducibility
+paramargs <- list(rep.num=1000, seed=261, ncores=4)
+
 tn = list("MC_anO2_lake1","TS_lake_2000","MC_anO2_lake2")
+
 ## set up for loop to iterate over list "tn"
 for (tmnt in tn) {
   ## make file and variable names
@@ -38,6 +40,25 @@ for (tmnt in tn) {
   spieceasi<-spiec.easi(otus, method='mb', lambda.min.ratio=0.01, nlambda=50, 
     sel.criterion='stars', pulsar.select=TRUE, pulsar.params=paramargs)
   saveRDS(spieceasi, file=out_RDS)
+  ## convert rds to csv
+  edgevals=symBeta(getOptBeta(spieceasi), mode="maxabs")
+  edgevalsm<-as.matrix(edgevals)
+  #re-get OTU names as lost in matrix conversion
+  rownames(edgevalsm) <- rownames(otus)
+  colnames(edgevalsm) <- rownames(otus)
+  ## convert matrix to adjacency graph
+  edgevals.adj<-graph.adjacency(
+    edgevalsm,mode="undirected",weighted=TRUE,diag=FALSE,add.colnames=NULL)
+  Evals_df<-get.data.frame(edgevals.adj)
+  ## change column name from "weight" to "SE weight"
+  colnames(Evals_df)[colnames(Evals_df) == "weight"] <- "spieceasi.weight"
+  ## make column with named edges
+  Evals_df$edge<-paste(Evals_df$from,"to",Evals_df$to,sep="_")
+  ## reorder columns so edge is first
+  Spieceasi_edges <- Evals_df[,c(4,3,1,2)]
+  ## save file as csv
+  outfile=paste0("results/",tmnt,"_Spieceasi_edges.csv")
+  write.csv(Spieceasi_edges, file=outfile, quote=F, row.names=F)
 }
 
 
