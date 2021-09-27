692e4012b21437176656853587d6f5600e693945
rmondav <12041885+rmondav@users.noreply.github.com>
2021-09-27 15:48:21 +0200

    combine slurm scripts and remove redundants

--- scripts/sbatch_network_calculations.sh
+++ scripts/sbatch_network_calculations.sh
@@ -1,15 +1,17 @@
 #! /bin/bash
 
 #SBATCH -A snic2020-5-529
-#SBATCH -J pearsNunion
-#SBATCH -t 00:40:00
-#SBATCH -p core -n 1
+#SBATCH -J netwrk_calcs
+#SBATCH -t 06:00:00
+#SBATCH -p core -n 4
 
 module load bioinfo-tools R/4.0.4 R_packages/4.0.4
 maindir="/proj/snic2020-16-196/nobackup/CHtrophy_network_nobackup/methanotrophy_networks"
-cd $maindir/
+cd $maindir/processed_tables/
+Rscript --no-restore --no-save "$maindir/scripts/SPIEC-EASI.R" &&
+cd ..
+Rscript --no-restore --no-save "$maindir/scripts/SPARCC.R" &&
 Rscript --no-restore --no-save "scripts/Pearson_correlation.R" &&
-Rscript --no-restore --no-save "scripts/make_unioned_tables.R"
+Rscript --no-restore --no-save "scripts/make_unioned_tables.R" &&
 Rscript --no-restore --no-save "scripts/process_OTU_tax_tables_for_plotting.R"
-
 
